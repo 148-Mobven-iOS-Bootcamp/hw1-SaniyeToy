0d2fb3c6e8864f7e7003c404d70b9a32266be368
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2021-12-17 16:30:24 +0300

    Create Project

---
+++ GitFlowCase/GitFlowCaseUITests/GitFlowCaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitFlowCaseUITestsLaunchTests.swift
+//  GitFlowCaseUITests
+//
+//  Created by MacOS on 17.12.2021.
+//
+
+import XCTest
+
+class GitFlowCaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
